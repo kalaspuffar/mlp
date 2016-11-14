4b764275ee86e6393c0f6e384693c3a3536b41f2
Daniel Persson <mailto.woden@gmail.com>
2016-11-14 07:00:20 +0100

    Simplistic image reader.

--- src/org/ea/mlp/ImageReader.java
+++ src/org/ea/mlp/ImageReader.java
@@ -1,4 +1,20 @@
 package org.ea.mlp;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class ImageReader {
+    public int[] readImage(String filename) {
+        try {
+            BufferedImage bi = ImageIO.read(new File(filename));
+            int[] data = bi.getData().getPixels(0, 0, bi.getWidth(), bi.getHeight(), new int[bi.getWidth() * bi.getHeight()]);
+            bi.flush();
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new int[] {};
+    }
 }
