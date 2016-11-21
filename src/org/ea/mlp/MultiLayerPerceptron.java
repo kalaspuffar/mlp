9284336679c69c4df60047186571465533ac2e9b
Daniel Persson <mailto.woden@gmail.com>
2016-11-21 22:34:13 +0100

    Implemented the full example with error and everything :)

--- src/org/ea/mlp/MultiLayerPerceptron.java
+++ src/org/ea/mlp/MultiLayerPerceptron.java
@@ -1,12 +1,8 @@
 package org.ea.mlp;
 
+import java.util.Arrays;
+import java.util.Map;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by woden on 2016-11-20.
- */
 public class MultiLayerPerceptron {
     float[] inputNeurons;
     float[] hiddenNeurons;
@@ -71,7 +67,7 @@ public class MultiLayerPerceptron {
         for(int i = 2; i <= hiddenL; i ++) {
             //for each one of these extra layers calculate their values
             for(int j = 0; j < hiddenN; j++) { //to
-                hiddenNeurons[(i-1)*hiddenN + j] = 0
+                hiddenNeurons[(i-1)*hiddenN + j] = 0;
 
                 for(int k = 0; k <hiddenN; k++) { //from
                     hiddenNeurons[(i-1)*hiddenN + j] += hiddenNeurons[(i-1)*hiddenN + k] * hiddenToHidden(i,k,j);
@@ -98,13 +94,163 @@ public class MultiLayerPerceptron {
     }
 
     //trains the network according to our parameters
-    public boolean trainNetwork(float teachingStep,float lmse,float momentum,int trainingFiles) {
+    public boolean trainNetwork(float teachingStep, float lmse, float momentum, Map<String, Integer> imageMap) {
+        float mse = 999.0f;
+        int epochs = 1;
+        float error = 0.0f;
+        //the delta of the output layer
+        float[] odelta = new float[outputN];
+        //the delta of each hidden layer
+        float[] hdelta = new float[hiddenN*hiddenL];
 
+        ImageReader imageReader = new ImageReader();
+
+        //used to keep the previous weights before modification, for momentum
+        prWeights = Arrays.copyOf(weights, weights.length);
+
+        int target = 0;
+        while(Math.abs(mse-lmse) > 0.0001) {
+            //for each epoch reset the mean square error
+            mse = 0.0f;
+
+            //for each file
+            for(Map.Entry<String, Integer> entry : imageMap.entrySet()) {
+                populateInput(imageReader.readImage(entry.getKey()));
+                target = entry.getValue();
+
+                //then calculate the network
+                calculateNetwork();
+
+                //Now we have calculated the network for this iteration
+                //let's back-propagate following the back-propagation algorithm
+                for(int i = 0; i < outputN; i ++) {
+                    //let's get the delta of the output layer
+                    //and the accumulated error
+                    if(i != target) {
+                        odelta[i] = (0.0f - outputNeurons[i])*dersigmoid(outputNeurons[i]);
+                        error += (0.0f - outputNeurons[i])*(0.0-outputNeurons[i]);
+                    } else {
+                        odelta[i] = (1.0f - outputNeurons[i])*dersigmoid(outputNeurons[i]);
+                        error += (1.0f - outputNeurons[i])*(1.0-outputNeurons[i]);
+                    }
+                }
+
+                //we start propagating backwards now, to get the error of each neuron
+                //in every layer let's get the delta of the last hidden layer first
+                for(int i = 0; i < hiddenN; i++) {
+                    hdelta[(hiddenL-1)*hiddenN+i] = 0; //zero the values from the previous iteration
+
+                    //add to the delta for each connection with an output neuron
+                    for(int j = 0; j < outputN; j ++)
+                    {
+                        hdelta[(hiddenL-1)*hiddenN+i] += odelta[j] * hiddenToOutput(i,j) ;
+                    }
+
+                    //The derivative here is only because of the
+                    //delta rule weight adjustment about to follow
+                    hdelta[(hiddenL-1)*hiddenN+i] *= dersigmoid(hiddenAt(hiddenL,i));
+                }
+
+                //now for each additional hidden layer, provided they exist
+                for(int i = hiddenL-1; i >0; i--) {
+                    //add to each neuron's hidden delta
+                    for(int j = 0; j < hiddenN; j ++) { //from
+                        hdelta[(i-1)*hiddenN+j] = 0;//zero the values from the previous iteration
+
+                        for(int k = 0; k <hiddenN; k++) { //to
+                            //the previous hidden layers delta multiplied by the weights
+                            //for each neuron
+                            hdelta[(i-1)*hiddenN+j] += hdelta[i*hiddenN+k] * hiddenToHidden(i+1,j,k);
+                        }
+
+                        //The derivative here is only because of the
+                        //delta rule weight adjustment about to follow
+                        hdelta[(i-1)*hiddenN+j] *= dersigmoid(hiddenAt(i,j));
+                    }
+                }
+
+                //Weights modification
+                tempWeights = Arrays.copyOf(weights, weights.length);//keep the previous weights somewhere, we will need them
+
+                //hidden to Input weights
+                for(int i = 0; i < inputN; i ++) {
+                    for(int j = 0; j < hiddenN; j ++) {
+                        weights[inputN*j+i] += momentum*(inputToHidden(i,j) - _prev_inputToHidden(i,j)) +
+                                teachingStep * hdelta[j] * inputNeurons[i];
+                    }
+                }
+
+                //hidden to hidden weights, provided more than 1 layer exists
+                for(int i = 2; i <=hiddenL; i++) {
+                    for(int j = 0; j < hiddenN; j ++) { //from
+                        for(int k =0; k < hiddenN; k ++) { //to
+                            weights[inputN*hiddenN+((i-2)*hiddenN*hiddenN)+hiddenN*j+k]
+                                += momentum*(hiddenToHidden(i,j,k) - _prev_hiddenToHidden(i,j,k)) +
+                                    teachingStep * hdelta[(i-1)*hiddenN+k] * hiddenAt(i-1,j);
+                        }
+                    }
+                }
+
+                //last hidden layer to output weights
+                for(int i = 0; i < outputN; i++) {
+                    for(int j = 0; j < hiddenN; j ++) {
+                        weights[(inputN*hiddenN + (hiddenL-1)*hiddenN*hiddenN + j*outputN+i)]
+                            += momentum*(hiddenToOutput(j,i) - _prev_hiddenToOutput(j,i)) +
+                                teachingStep * odelta[i] * hiddenAt(hiddenL,j);
+                    }
+                }
+
+                prWeights = tempWeights;
+
+                //add to the total mse for this epoch
+                mse += error/(outputN+1);
+                //zero out the error for the next iteration
+                error = 0;
+            }
+
+//            if(epochs > 1000) break;
+//            epochs++;
+        }
+        return true;
     }
 
     //recalls the network for a given bitmap file
-    public void recallNetwork(int fileNum) {
+    public void recallNetwork(String filename, int[] data) {
+        //first populate the input neurons
+        populateInput(data);
 
+        //then calculate the network
+        calculateNetwork();
+
+        float winner = -1;
+        int index = 0;
+
+        //find the best fitting output
+        for(int i =0; i < outputN; i++)
+        {
+            if(outputNeurons[i] > winner)
+            {
+                winner = outputNeurons[i];
+                index = i;
+            }
+        }
+
+        //output it
+        System.out.println("The neural network thinks that image " + filename + " represents a \n\r\n\r \t\t----->| " + index + " |<------\t\t \n\r\n\r");
+
+        //now let's the exact percentages of what it thanks
+        System.out.println(
+                "Analyzing what the network thinks about bitmap " + filename + " we see that it thinks it is: \n\r" +
+                "| 0 with " + (int)(outputNeurons[0]*100) + " probability |\n\r" +
+                "| 1 with " + (int)(outputNeurons[1]*100) + " probability |\n\r" +
+                "| 2 with " + (int)(outputNeurons[2]*100) + " probability |\n\r" +
+                "| 3 with " + (int)(outputNeurons[3]*100) + " probability |\n\r" +
+                "| 4 with " + (int)(outputNeurons[4]*100) + " probability |\n\r" +
+                "| 5 with " + (int)(outputNeurons[5]*100) + " probability |\n\r" +
+                "| 6 with " + (int)(outputNeurons[6]*100) + " probability |\n\r" +
+                "| 7 with " + (int)(outputNeurons[7]*100) + " probability |\n\r" +
+                "| 8 with " + (int)(outputNeurons[8]*100) + " probability |\n\r" +
+                "| 9 with " + (int)(outputNeurons[9]*100) + " probability |\n\r");
     }
 
     private float inputToHidden(int inp, int hid) {
@@ -134,20 +280,12 @@ public class MultiLayerPerceptron {
         return hiddenNeurons[(layer-1)*hiddenN + hid];
     }
 
-    /*helper macros to locate the appropriate neuron's delta*/
-    private float outputDeltaAt(out) {
-        return (*(odelta+out));
-    }
-    private float hiddenDeltaAt(layer,hid) {
-        return (*(hdelta+(layer-1)*hiddenN+hid));
-    }
-
     /*math help*/
     private float sigmoid(float value) {
         return (float)(1f/(1f+Math.exp(-value)));
     }
     private float dersigmoid(float value) {
-        return (value*(1f-value))
+        return (value*(1f-value));
     }
 
 }
--- src/org/ea/mlp/TestApplication.java
+++ src/org/ea/mlp/TestApplication.java
@@ -1,4 +1,58 @@
 package org.ea.mlp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestApplication {
+    static Map<String, Integer> imageMap = new HashMap<String, Integer>();
+
+    public static void main(String[] arg) {
+        imageMap.put("images/img0.bmp", 1);
+        imageMap.put("images/img1.bmp", 2);
+        imageMap.put("images/img2.bmp", 7);
+        imageMap.put("images/img3.bmp", 3);
+        imageMap.put("images/img4.bmp", 4);
+        imageMap.put("images/img5.bmp", 5);
+        imageMap.put("images/img6.bmp", 6);
+        imageMap.put("images/img7.bmp", 7);
+        imageMap.put("images/img8.bmp", 8);
+        imageMap.put("images/img9.bmp", 9);
+        imageMap.put("images/img10.bmp", 4);
+        imageMap.put("images/img11.bmp", 2);
+        imageMap.put("images/img12.bmp", 1);
+        imageMap.put("images/img13.bmp", 3);
+        imageMap.put("images/img14.bmp", 5);
+        imageMap.put("images/img15.bmp", 9);
+        imageMap.put("images/img16.bmp", 6);
+        imageMap.put("images/img17.bmp", 8);
+        imageMap.put("images/img18.bmp", 7);
+        imageMap.put("images/img19.bmp", 0);
+        imageMap.put("images/img20.bmp", 2);
+        imageMap.put("images/img21.bmp", 0);
+
+        MultiLayerPerceptron mlp = new MultiLayerPerceptron(24, 3);
+        if(!mlp.trainNetwork(0.04f, 0.01f, 0.4f, imageMap)) {
+            System.out.println("There was an error while training ... Quitting\n\r");
+            System.exit(0);
+        }
+
+        int mb = 1024*1024;
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("##### Heap utilization statistics [MB] #####");
+        System.out.println("Used Memory:" + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+        System.out.println("Free Memory:" + runtime.freeMemory() / mb);
+        System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+        System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+
+        ImageReader ir = new ImageReader();
+        mlp.recallNetwork("images/img19.bmp", ir.readImage("images/img19.bmp"));
+
+        /*
+        hiddenNeurons = 24
+        hiddenLayers = 3
+        teachingStep = 0.04
+        leastMSE = 0.01
+        momentum = 0.4
+        */
+    }
 }
